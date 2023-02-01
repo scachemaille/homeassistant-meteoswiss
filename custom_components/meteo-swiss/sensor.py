a28f33d8deb083f08d0eb09238366cb2d7db5c87
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-02-01 09:17:26 +0000

    Blacken code and add error handling for exceptional condition.
    Please update to this version and report your logs if you experience issues.

--- custom_components/meteo-swiss/sensor.py
+++ custom_components/meteo-swiss/sensor.py
@@ -1,8 +1,9 @@
-from hamsclientfork import meteoSwissClient
-
 import logging
+import pprint
 
+from hamsclientfork import meteoSwissClient
 from homeassistant.helpers.entity import Entity
+
 
 from .const import (
     DOMAIN,
@@ -11,58 +12,56 @@ from .const import (
     SENSOR_TYPE_ICON,
     SENSOR_TYPE_NAME,
     SENSOR_TYPE_UNIT,
-    SENSOR_TYPES,
     SENSOR_DATA_ID,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(hass, config, async_add_entities):
-    _LOGGER.info("Starting asnyc setup platform")
-    client = hass.data[DOMAIN]['client']
-  
+    _LOGGER.debug("Starting async setup platform")
+    client = hass.data[DOMAIN]["client"]
 
     async_add_entities(
-        [
-            MeteoSwissSensor(sensor_type, client)
-            for sensor_type in SENSOR_TYPES
-        ],
+        [MeteoSwissSensor(typ, client) for typ in SENSOR_TYPES],
         True,
     )
 
-class MeteoSwissSensor(Entity):
 
-    def __init__(self,sensor_type,client:meteoSwissClient):
+class MeteoSwissSensor(Entity):
+    def __init__(self, sensor_type, client: meteoSwissClient):
         self._client = client
         if client is None:
             _LOGGER.error("Error empty client")
-        self._state = None 
+        self._state = None
         self._type = sensor_type
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self._data['name']} {SENSOR_TYPES[self._type][SENSOR_TYPE_NAME]}"
-    
+        x = SENSOR_TYPES[self._type][SENSOR_TYPE_NAME]
+        return f"{self._data['name']} {x}"
+
     @property
     def unique_id(self):
         """Return the unique id of the sensor."""
         return self.name
+
     @property
     def state(self):
-        
+
         dataId = SENSOR_TYPES[self._type][SENSOR_DATA_ID]
         try:
-            return self._data['condition'][0][dataId]
-        except:
-            _LOGGER.debug("Unable to return data for : %s"%(self._data['condition'][0][dataId]))
+            return self._data["condition"][0][dataId]
+        except Exception as exc:
+            _LOGGER.warning("self._data['condition'][0][dataId]: %s:", exc)
+            _LOGGER.warning("self._data: %s: %s", pprint.pformat(self._data))
             return None
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
         return SENSOR_TYPES[self._type][SENSOR_TYPE_UNIT]
-    
 
     @property
     def icon(self):
@@ -75,6 +74,5 @@ class MeteoSwissSensor(Entity):
         return SENSOR_TYPES[self._type][SENSOR_TYPE_CLASS]
 
     def update(self):
-        #self._client.update()
+        # self._client.update()
         self._data = self._client.get_data()
-        
